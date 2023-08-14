ff65644ab193be115543e10a4040eec8d17b4e9e
Astien <astienth@gmail.com>
2023-08-14 23:00:51 +0200

    initial commit

---
+++ 7Days_bhaptics.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+using MyBhapticsTactsuit;
+
+namespace Days_bhaptics
+{
+    [BepInPlugin("org.bepinex.plugins.7Days_bhaptics", "7Days_bhaptics integration", "1.4")]
+    public class Plugin : BaseUnityPlugin
+    {
+#pragma warning disable CS0109 // Remove unnecessary warning
+        internal static new ManualLogSource Log;
+#pragma warning restore CS0109
+        public static TactsuitVR tactsuitVr;
+
+        private void Awake()
+        {
+            // Make my own logger so it can be accessed from the Tactsuit class
+            Log = base.Logger;
+            // Plugin startup logic
+            Logger.LogMessage("Plugin 7Days_bhaptics is loaded!");
+            tactsuitVr = new TactsuitVR();
+            // one startup heartbeat so you know the vest works correctly
+            tactsuitVr.PlaybackHaptics("HeartBeat");
+            // patch all functions
+            var harmony = new Harmony("bhaptics.patch.7days");
+            harmony.PatchAll();
+        }
+    }
+    /*
+    [HarmonyPatch(typeof(Food), "OnEat", new Type[] { })]
+    public class bhaptics_OnEat
+    {
+        [HarmonyPostfix]
+        public static void Postfix()
+        {
+            if (Plugin.tactsuitVr.suitDisabled)
+            {
+                return;
+            }
+            Plugin.tactsuitVr.PlaybackHaptics("eatingvisor");
+            Plugin.tactsuitVr.PlaybackHaptics("Eating");
+        }
+    }
+    */
+}
+
